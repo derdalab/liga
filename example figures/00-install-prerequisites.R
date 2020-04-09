f38a2c3027163bae7de23b598cc4dd05e2043e16
derdalab <56060054+derdalab@users.noreply.github.com>
2020-04-09 14:41:41 -0600

    Update 00-install-prerequisites.R

--- example figures/00-install-prerequisites.R
+++ example figures/00-install-prerequisites.R
@@ -1,18 +1,24 @@
 # install packages used by the other R scripts in this directory
 
+# choose the install library - default to first user library
+install.lib <- sub(":.*$", "", Sys.getenv("R_LIBS_USER"))
+
+# create the install library if it does not already exist
+dir.create(path = install.lib, showWarnings = FALSE, recursive = TRUE)
+
 # edgeR is distributed through BioConductor - install it if necessary, then get edgeR
 # https://bioconductor.org/packages/release/bioc/html/edgeR.html
 if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-BiocManager::install("edgeR")
+    install.packages("BiocManager", lib = install.lib)
+BiocManager::install("edgeR", lib = install.lib)
 
 # Rmarkdown/knitr uses a TeX install
-install.packages('tinytex')
+install.packages('tinytex", lib = install.lib)
 tinytex::install_tinytex()
 # to uninstall TinyTeX, run tinytex::uninstall_tinytex() 
 
 # install packages from CRAN
-install.packages("ggplot2")
-install.packages("drc")		       # dose-response curve package
-install.packages("rmarkdown")    # used for report generation
-#install.packages("tidyverse")
+install.packages("ggplot2", lib = install.lib)
+install.packages("rmarkdown", lib = install.lib)    # used for report generation
+install.packages("drc", lib = install.lib)          # dose-response curve package
+#install.packages("tidyverse", lib = install.lib)
