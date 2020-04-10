c3a6f72fd4f6febbf860e3cca248623c4f2d1a02
derdalab <56060054+derdalab@users.noreply.github.com>
2020-04-10 11:07:39 -0600

    minor code clean-up

--- example figures/00-install-prerequisites.R
+++ example figures/00-install-prerequisites.R
@@ -1,5 +1,8 @@
-# install missing packages used by the other R scripts in this directory
-# if many package dependancies need installation this can take ~45 minutes
+# Install missing packages used by the other R scripts in this directory.
+#
+# WARNING: If many package dependancies need installation this can take ~45 minutes.
+# WARNING: If you mantain specific versisons of libraries, review this script
+#          before overwriting your existing install.
 
 # choose the install library - default to first user library
 install.lib <- sub(":.*$", "", Sys.getenv("R_LIBS_USER"))
@@ -11,19 +14,28 @@ dir.create(path = install.lib, recursive = TRUE, showWarnings = FALSE)
 # https://bioconductor.org/packages/release/bioc/html/edgeR.html
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager", lib = install.lib)
-if(!"edgeR" %in% installed.packages()[,"Package"])
+if(!"edgeR" %in% installed.packages())
     BiocManager::install("edgeR", lib = install.lib)
 
 # knitr/Rmarkdown, install it after tinytex (used of knitr PDF output)
-if(!"tinytex" %in% installed.packages()[,"Package"])
+if(!"tinytex" %in% installed.packages())
     install.packages("tinytex", lib = install.lib)
 tinytex::install_tinytex()  # to uninstall TinyTeX, run tinytex::uninstall_tinytex() 
-if(!"rmarkdown" %in% installed.packages()[,"Package"])
+if(!"rmarkdown" %in% installed.packages())
     install.packages("rmarkdown", lib = install.lib)    # used for report generation
 
-# install packages from CRAN if they are not already installed
-if(!"ggplot2" %in% installed.packages()[,"Package"])
+# ggplot2 - widely used plot package
+if(!"ggplot2" %in% installed.packages())
     install.packages("ggplot2", lib = install.lib)      # common graphing package
-if(!"drc" %in% installed.packages()[,"Package"])
-    install.packages("drc", lib = install.lib)          # dose-response curve package
+
+# dose-response curve package
+if(!"drc" %in% installed.packages()){
+    if(!"pbkrtest" %in% installed.packages()){
+        # install an older version of pbkrtest (drc requirement) for greater version compatiblity
+        pbkrtest.url <- "http://cran.r-project.org/src/contrib/Archive/pbkrtest/pbkrtest_0.4-7.tar.gz"
+        install.packages(pbkrtest.url, repos = NULL, type = "source", lib = install.lib)
+    }
+    install.packages("drc", lib = install.lib)
+}
+
 #install.packages("tidyverse", lib = install.lib)
