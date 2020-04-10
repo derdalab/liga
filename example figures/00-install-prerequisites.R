a9fbac1b5ef8aabcdc5bb9aef977f12863a032ff
derdalab <56060054+derdalab@users.noreply.github.com>
2020-04-10 10:41:39 -0600

    skip installs if already installed

--- example figures/00-install-prerequisites.R
+++ example figures/00-install-prerequisites.R
@@ -1,25 +1,29 @@
-# install packages used by the other R scripts in this directory
+# install missing packages used by the other R scripts in this directory
+# if many package dependancies need installation this can take ~45 minutes
 
 # choose the install library - default to first user library
 install.lib <- sub(":.*$", "", Sys.getenv("R_LIBS_USER"))
 
 # create the install library directories if they do not already exist
-dir.create(path = install.lib, showWarnings = FALSE, recursive = TRUE)
+dir.create(path = install.lib, recursive = TRUE, showWarnings = FALSE)
 
 # edgeR is distributed through BioConductor - install it if necessary, then get edgeR
 # https://bioconductor.org/packages/release/bioc/html/edgeR.html
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager", lib = install.lib)
-library(BiocManager)
-BiocManager::install("edgeR", lib = install.lib)
+if(!"edgeR" %in% installed.packages()[,"Package"])
+    BiocManager::install("edgeR", lib = install.lib)
 
-# Rmarkdown/knitr uses a TeX install
-install.packages("tinytex", lib = install.lib)
-tinytex::install_tinytex()
-# to uninstall TinyTeX, run tinytex::uninstall_tinytex() 
+# knitr/Rmarkdown, install it after tinytex (used of knitr PDF output)
+if(!"tinytex" %in% installed.packages()[,"Package"])
+    install.packages("tinytex", lib = install.lib)
+tinytex::install_tinytex()  # to uninstall TinyTeX, run tinytex::uninstall_tinytex() 
+if(!"rmarkdown" %in% installed.packages()[,"Package"])
+    install.packages("rmarkdown", lib = install.lib)    # used for report generation
 
-# install packages from CRAN
-install.packages("ggplot2", lib = install.lib)
-install.packages("rmarkdown", lib = install.lib)    # used for report generation
-install.packages("drc", lib = install.lib)          # dose-response curve package
+# install packages from CRAN if they are not already installed
+if(!"ggplot2" %in% installed.packages()[,"Package"])
+    install.packages("ggplot2", lib = install.lib)      # common graphing package
+if(!"drc" %in% installed.packages()[,"Package"])
+    install.packages("drc", lib = install.lib)          # dose-response curve package
 #install.packages("tidyverse", lib = install.lib)
